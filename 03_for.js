42a26c90aa16a5ac581aa705f46b1b54d589338c
佐藤佳幸 <satouyoshiyuki@satouyoshiyuki-no-MacBook-Pro.local>
2021-04-17 13:15:19 +0900

    3回目の課題

---
+++ 03_for.js
@@ -0,0 +1,50 @@
+/**
+ * 問1. 1~10の数字をforを使用して、consoleにて表示してください
+ */
+for(let i=1; i<=10; i++){
+  console.log(i);
+}
+
+
+/**
+ * 問2. -1~-10の数字をforを使用して、consoleにて表示してください
+ */
+for(let i=-10; i<=-1; i++){
+  console.log(i);
+}
+
+
+
+/**
+ * 問3. 1~20の数字をforを使用して、2の倍数のみconsoleにて表示してください
+ */
+for(let i=1; i<=20; i++){
+  if(i%2===0){
+    console.log(i);
+  }
+}
+
+
+/**
+ * 問4. for文とは何か、説明してください
+ */
+
+/**
+ * 問4. 回答欄
+ * 繰り返し処理を実行するための構文。
+ * 
+ */
+
+
+
+/**
+ * 問5. for文を使用して、配列test5Arrayの中身を変数で格納し、
+ * 変数をconsoleで表示してください
+ * 参考: https://maku77.github.io/js/array/loop.html
+ */
+const test5Array = ['test', 'dummy', 'sample', 'hoge', 'huga'];
+
+for(let i=0; i<1; i++){
+  console.log(test5Array);
+}
+
